8752bb3da221ebd1d2b6a69ba4db0158b49a0d24
Admin <admin@ptr-g7fkvf0j2jc7ezonffe.18120a2.ip6.access.telenet.be>
2017-08-21 20:50:22 +0300

    binnenhalen joon en omzetten naar core data afgewerkt

--- werkstuk2Ios/ViewController.swift
+++ werkstuk2Ios/ViewController.swift
@@ -64,21 +64,63 @@ class ViewController: UIViewController {
                 
                 
             
+                //init belangrijkebomen
+                let opmerkelijkeBomen = NSEntityDescription.insertNewObject(forEntityName: "OpmerkelijkeBomen", into: context) as! OpmerkelijkeBomen
                 
-            
-                let opmerkelijkeBomen = OpmerkelijkeBomen(context: context)
                 
                 opmerkelijkeBomen.nhits = json.value(forKey: "nhits") as! int_fast16_t
                 
-                let records = json.value(forKey: "records") as! [[String : Any]]
-            
-                let record = NSArray()
+                //init parameters
+                let parameter = NSEntityDescription.insertNewObject(forEntityName: "Parameter", into: context) as! Parameter
                 
+                let parameterjson = json.value(forKey: "parameters") as! [String: Any]
+                
+                //parameters aanvullen
+                parameter.timezone = parameterjson["timezone"] as? String
+                parameter.rows = parameterjson["rows"] as! Int16
+                parameter.format = parameterjson["format"] as? String
+                
+                //toevoeging parameters
+                opmerkelijkeBomen.parameters = parameter
+                
+                
+                //init records
+                let records = json.value(forKey: "records") as! [[String : Any]]
+                
+                
+                let recordSet = NSSet()
+                
+                
+                //records aanvullen
                 for recordjson in records{
-                    let fields = recordjson["fields"] as! [[String: Any]]
+                    let record = NSEntityDescription.insertNewObject(forEntityName: "Record", into: context) as! Record
+                    
+                    record.datasetid = recordjson["datasetid"] as? String
+                    record.recordid = recordjson["recordid"] as? String
+                    record.record_timestamp = recordjson["record_timestamp"] as? String
                     
                     
+                    let fieldsjson = recordjson["fields"] as! [String: Any]
+                    
+                    let field = NSEntityDescription.insertNewObject(forEntityName: "Field", into: context) as! Field
+                    
+                    field.status = fieldsjson["status"] as? String
+                    field.omtrek = fieldsjson["omtrek"] as! Int16
+                    field.straat = fieldsjson["straat"] as? String
+                    field.beplanting = fieldsjson["beplanting"] as? String
+                    field.diameter_van_de_kroon = fieldsjson["diameter_van_de_kroon"] as! Int16
+                    field.soort = fieldsjson["soort"] as? String
+                    field.positie = fieldsjson["positie"] as? String
+                    field.id = fieldsjson["id"] as! Int16
+                    field.gemeente = fieldsjson["gemeente"] as? String
+                    
+                    record.fielden = field
+                    
+                    recordSet.adding(record)
                 }
+                
+                opmerkelijkeBomen.records = recordSet
+                
                 
             } catch  {
                 print("error trying to convert data to JSON")
